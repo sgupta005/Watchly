c442e51fc77de359145b6dc1169f0c3a536ac481
Shivam Gupta <shivamgupta02005@gmail.com>
2024-05-16 20:40:36 +0530

    Attempt to fix: Build issues

--- providers/auth-provider.tsx
+++ providers/auth-provider.tsx
@@ -1,43 +1,45 @@
-"use client";
 import { getUserDetails } from "@/app/dashboard/_actions/actions";
 import { useUser } from "@clerk/nextjs";
 import React, { createContext, useState, useEffect } from "react";
+
 const AuthContext = createContext<any>(null);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const { user } = useUser();
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState<boolean>(true);
   const [userDetails, setUserDetails] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const checkAuth = async () => {
+    const fetchUserDetails = async () => {
       try {
         setLoading(true);
-        if (!user) {
-          return;
-        }
-        console.log(user);
+        if (!user) return;
+
         const response = await getUserDetails({
           name: user.fullName!,
           email: user.primaryEmailAddress?.emailAddress!,
         });
+
         if (!response) {
-          console.log("No user details found");
+          setError("No user details found");
           return;
         }
+
         setUserDetails(response);
       } catch (error) {
+        setError("Error fetching user details");
         console.error("Error fetching user:", error);
       } finally {
         setLoading(false);
       }
     };
 
-    checkAuth();
+    fetchUserDetails();
   }, [user]);
 
   return (
-    <AuthContext.Provider value={{ userDetails, loading }}>
+    <AuthContext.Provider value={{ userDetails, loading, error }}>
       {children}
     </AuthContext.Provider>
   );
