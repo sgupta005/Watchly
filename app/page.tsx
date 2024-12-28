d4d45ef83e5bbb72c66accf3f0e48c732a001912
Shivam Gupta <shivamgupta02005@gmail.com>
2024-12-28 14:52:23 +0530

    removed testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main className="min-h-screen">
       <Hero />
       <Features />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Footer />
     </main>
   );
